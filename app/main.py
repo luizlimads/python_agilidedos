f627c82ad3557074596d41eb35752c3987443f6f
luizlimads <luizhenrique.limads@gmail.com>
2023-09-05 20:21:32 -0300

    feat: create route login

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 
-def create_app(testing: bool = True):
+def create_app():
     app = Flask(__name__)
 
     @app.route('/')
     def index():
+        return render_template('home.html', apelido='apelido')
+
+    @app.route('/login')
+    def login():
         return render_template('login.html')
-    
+
+    @app.route('/verifica', methods=['POST',])
+    def verifica():
+        apelido = request.form['apelido']
+        print(apelido)
+        return redirect('/')
+
     return app
