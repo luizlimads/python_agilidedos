7f8299a5c1ba5329657b38cc924af543d9452697
luizlimads <luizhenrique.limads@gmail.com>
2023-09-05 16:59:13 -0300

    feat: add login template

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 def create_app(testing: bool = True):
     app = Flask(__name__)
 
     @app.route('/')
     def index():
-        return f'Hello, World!: {testing}'
+        return render_template('login.html')
     
     return app
