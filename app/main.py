a5da6fb22decce0eb26ef7b4e589b9782c73f7f4
luizlimads <luizhenrique.limads@gmail.com>
2023-09-08 12:52:46 -0300

    test: alter main file, remove create_app

--- app/main.py
+++ app/main.py
@@ -1,20 +1,17 @@
 from flask import Flask, render_template, redirect, request
 
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    @app.route('/')
-    def index():
-        return render_template('home.html', apelido='apelido')
+@app.route('/')
+def index():
+    return render_template('home.html', apelido='apelido')
 
-    @app.route('/login')
-    def login():
-        return render_template('login.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
-    @app.route('/verifica', methods=['POST',])
-    def verifica():
-        apelido = request.form['apelido']
-        print(apelido)
-        return redirect('/')
-
-    return app
+@app.route('/verifica', methods=['POST',])
+def verifica():
+    apelido = request.form['apelido']
+    print(apelido)
+    return redirect('/')
